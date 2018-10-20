b06a04d6125e01487d1cca03281e578ada9e2078
sq1fya <rgpszczecin@gmail.com>
2018-10-20 17:30:27 +0200

    add ternary operator

--- src/components/counter.js
+++ src/components/counter.js
@@ -3,13 +3,18 @@ import React, { Component } from "react";
 class Counter extends Component {
   
   state = {
-    count: 1
-  }
+    count: 0
+  };
   
+  formatCount() {
+    const { count } = this.state
+    return count === 0 ? "Zero" : count;
+  }
+
   render() {
     return (
       <div>
-        <span>{this.state.count}</span>
+        <span>{this.formatCount()}</span>
         <button>Increment</button>
       </div>
     );
