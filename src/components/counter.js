988ee55253f6248a72806fd340cbe0fc9d3dc475
sq1fya <rgpszczecin@gmail.com>
2018-10-20 17:56:01 +0200

    Map table element and list it

--- src/components/counter.js
+++ src/components/counter.js
@@ -1,21 +1,29 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-  
   state = {
-    count: 0
+    count: 1,
+    tags: ['tag1', 'tag2', 'tag3']
   };
-  
+
   formatCount() {
-    const { count } = this.state
+    const { count } = this.state;
     return count === 0 ? "Zero" : count;
+  }
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
   }
 
   render() {
     return (
       <div>
-        <span>{this.formatCount()}</span>
-        <button>Increment</button>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
+      <ul>
+        {this.state.tags.map( tag => <li>{tag}</li>)}
+      </ul>
       </div>
     );
   }
