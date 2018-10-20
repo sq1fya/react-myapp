15faae4899b4e4504867532eb8d02bb1433f52e9
sq1fya <rgpszczecin@gmail.com>
2018-10-20 18:27:59 +0200

    Add function increment to button

--- src/components/counter.js
+++ src/components/counter.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {
     count: 1,
-    tags: ['tag1', 'tag2', 'tag3']
+    tags: []
   };
 
   formatCount() {
@@ -16,14 +16,34 @@ class Counter extends Component {
     return classes;
   }
 
+  renderTags() {
+    if (this.state.tags.length === 0) return <p>There are no tags!</p>;
+
+    return (
+      <ul>
+        {this.state.tags.map(tag => (
+          <li key={tag}>{tag}</li>
+        ))}
+      </ul>
+    );
+  }
+
+  handleIncrement = () => {
+    this.setState({ count: this.state.count + 1})
+  }
+
   render() {
     return (
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
-      <ul>
-        {this.state.tags.map( tag => <li>{tag}</li>)}
-      </ul>
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+        {this.state.tags.length === 0 && "Please create a new Tag!"}
+        {this.renderTags()}
       </div>
     );
   }
