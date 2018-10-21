4646af755098656486ccbff38383b605ed135b67
sq1fya <rgpszczecin@gmail.com>
2018-10-21 19:53:51 +0200

    Usage props

--- src/components/counter.js
+++ src/components/counter.js
@@ -2,17 +2,17 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 1,
+    value: this.props.value,
     tags: []
   };
 
   formatCount() {
-    const { count } = this.state;
-    return count === 0 ? "Zero" : count;
+    const { value } = this.state;
+    return value === 0 ? "Zero" : value;
   }
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
+    classes += this.state.value === 0 ? "warning" : "primary";
     return classes;
   }
 
@@ -29,10 +29,12 @@ class Counter extends Component {
   }
 
   handleIncrement = (product) => {
-    this.setState({ count: this.state.count + 1 });
+    this.setState({ value: this.state.value + 1 });
   };
 
   render() {
+
+
     return (
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
