39a9812da19eae178c6930eadb7c0a6b9ced73e3
sq1fya <rgpszczecin@gmail.com>
2018-10-21 19:45:31 +0200

    Using map to list from table

--- src/components/counters.js
+++ src/components/counters.js
@@ -2,12 +2,18 @@ import React, { Component } from 'react';
 import Counter from './counter'
 
 class Counters extends Component {
-    state = {  }
+    state = { 
+        counters: [
+            { id: 1, vale: 0},
+            { id: 2, vale: 0},
+            { id: 3, vale: 0},
+            { id: 4, vale: 0}
+        ]
+     }
     render() { 
         return (
             <div>
-                <Counter />
-                <Counter />
+                { this.state.counters.map(counter => <Counter key={counter.id} />)}
             </div>
           );
     }
